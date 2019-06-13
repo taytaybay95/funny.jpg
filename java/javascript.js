2d6c4bbb9fffd4c6bce843e61abad2441a75b760
taytaybay95 <51662527+taytaybay95@users.noreply.github.com>
2019-06-13 03:59:57 -0700

    changes

--- java/javascript.js
+++ java/javascript.js
@@ -1,1 +1,25 @@
-alert ( 'HELLO NEW WORLD');
+// WHEN THE USER CLICKS ON THE BUTTON. 
+//TOGGLE BETWEEN HIDING AND SHOWING THE DROPDOWN CONTENT
+
+
+function myFunction(){
+	document.getElementById("myDropdown").classList.toggle
+	("show");
+}
+
+
+//CLOSE THE DROPDOWN MENU IF THE USER CLICKS OUTSIDE OF IT
+
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns =
+	docuument.getElementsByClassName("dropdown-content");
+	var i;
+	for (i = 0; i < dropdowns.length; i++) {
+		var openDropdown = dropdowns[i];
+		if (openDropdown.classList.contains('show')) {
+			openDropdown.classList.remove('show');
+	  }
+	}
+  }
+}
